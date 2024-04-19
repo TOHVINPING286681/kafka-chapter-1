351a1ddfe1fc15a50c1fe11f7b4a703b422fbd15
TOHVINPING286681 <vinping0826@gmail.com>
2024-04-19 17:59:04 +0800

    .

--- src/main/java/com/codefarm/EventProducer.java
+++ src/main/java/com/codefarm/EventProducer.java
@@ -1,2 +1,30 @@
-package com.codefarm;public class EventProducer {
+package com.codefarm;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class EventProducer {
+//1. configs - broker, serializer(key, value)
+    boolean sendEvent(String message){
+        Properties kafkaProducerConfig = new Properties();
+
+        kafkaProducerConfig.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+        kafkaProducerConfig.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        kafkaProducerConfig.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class);
+//2. record
+        ProducerRecord<String,String>record = new ProducerRecord<>("my-first-topic",message);//topic is the place in 2.13 to show the result
+//3. Producer Interface
+        KafkaProducer<String,String>producer = new KafkaProducer<String, String>(kafkaProducerConfig);
+//4. send the record to cluster
+
+        producer.send(record);
+        //5. flush/close
+        producer.flush();
+        producer.close();
+     return true;
+    }
 }
--- src/test/java/com/codefarm/EventProducerTest.java
+++ src/test/java/com/codefarm/EventProducerTest.java
@@ -1,2 +1,15 @@
-package com.codefarm;public class EventProducerTest {
+package com.codefarm;
+
+import org.junit.jupiter.api.Test;
+
+public class EventProducerTest {
+
+    EventProducer eventProducer= new EventProducer();
+
+    @Test
+    void sendEventTest(){
+        eventProducer.sendEvent("123 My first event sss");
+
+
+    }
 }
